000de32bd30addfecbfa7057068598035ff3208e
Mau <mauricio.chaves.chaves@est.una.ac.cr>
2024-11-17 14:53:33 -0600

    doc test

--- bies/biesC/test/runTest.js
+++ bies/biesC/test/runTest.js
@@ -5,6 +5,20 @@ import fs from 'fs';
 import { exec } from 'child_process';
 import chalk from 'chalk';
 
+/**
+ * Script para ejecutar pruebas en archivos .bies dentro del directorio de pruebas.
+ * Este script ejecuta cada archivo .bies en el orden que se encuentra en el directorio `battery` y
+ * muestra el resultado en la consola. Usa `exec` para correr el script `biesC.js` con cada archivo .bies.
+ *
+ * @version 1.0.0
+ * 
+ * @author
+ * - Joshua Yarit Amador Lara
+ * - Mauricio Chaves Chaves [Coordinador]
+ * - Fabiola Rojas Alvarado
+ * - Eddy Villarreal Muñoz
+ */
+
 // Obtener la ruta del archivo actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,14 +26,19 @@ const __dirname = path.dirname(__filename);
 // Directorio de pruebas
 const testDirectory = path.join(__dirname, '../battery');
 
+/**
+ * Lee el directorio de pruebas y ejecuta los archivos .bies secuencialmente.
+ */
 fs.readdir(testDirectory, (err, files) => {
-  if (err)
-    throw err;
+  if (err) throw err;
 
-  // Filtra solo archivos .basm
+  // Filtra solo archivos .bies
   const basmFiles = files.filter(file => file.trim().endsWith('.bies'));
 
-  // Función recursiva para ejecutar archivos en orden
+  /**
+   * Ejecuta un archivo de prueba .bies y llama recursivamente a sí mismo para ejecutar el siguiente.
+   * @param {number} index - Índice del archivo actual en la lista de archivos .bies.
+   */
   const runFile = (index) => {
     if (index >= basmFiles.length) return;
 
